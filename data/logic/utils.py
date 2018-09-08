65024b3a67619a91af95333329d96c0f927fd850
Mardi <mango.alien@icloud.com>
2018-09-08 14:39:09 +1000

    example yaml and read code
    Signed-off-by: Mardi <mango.alien@icloud.com>

--- data/logic/utils.py
+++ data/logic/utils.py
@@ -3,10 +3,10 @@ import os.path as osp
 import os
 import pandas as pd
 
-def loadPopulationCSV():
-	path = 'raw/Population-projections-ACT.csv'
-	path = osp.normpath(osp.abspath(path))
+normabs = lambda x: osp.normpath(osp.abspath(x))
 
+def loadPopulationCSV():
+	path = normabs('raw/Population-projections-ACT.csv')
 	assert osp.isfile(path), path
 
 	df = pd.read_csv(path)
--- data/main.py
+++ data/main.py
@@ -1,6 +1,7 @@
 
 from logic import utils
-
+from argparse import Namespace
+import yaml
 
 
 if __name__ == '__main__':
@@ -9,3 +10,20 @@ if __name__ == '__main__':
 
 	df = utils.loadPopulationCSV()
 	print(df.head())
+
+	path = utils.normabs('raw/example.yml')
+	with open(path, 'r') as f:
+		y = yaml.load(f)
+
+	print '\n\n\n\n'
+	print y
+
+	print '\n\n\n\n'
+	for postcode, data in y.items():
+		print
+		print 'postcode:', postcode 
+
+		years = data['by_year']
+		for year in years:
+			ns = Namespace(**year)
+			print '-', ns.year, ns.aged_pct
