8d5f3822fa92f60deec2c9881794939ca18b57fe
Mardi <mango.alien@icloud.com>
2018-09-08 14:16:56 +1000

    population data reading into dataframe
    Signed-off-by: Mardi <mango.alien@icloud.com>

--- data/logic/utils.py
+++ data/logic/utils.py
@@ -1,4 +1,16 @@
 
+import os.path as osp
+import os
+import pandas as pd
+
+def loadPopulationCSV():
+	path = 'raw/Population-projections-ACT.csv'
+	path = osp.normpath(osp.abspath(path))
+
+	assert osp.isfile(path), path
+
+	df = pd.read_csv(path)
+	return df
 
 def test():
     return 'test test gh18-p77'
--- data/main.py
+++ data/main.py
@@ -1,6 +1,11 @@
 
 from logic import utils
 
+
+
 if __name__ == '__main__':
 	print('test')
 	print(utils.test())
+
+	df = utils.loadPopulationCSV()
+	print(df.head())
